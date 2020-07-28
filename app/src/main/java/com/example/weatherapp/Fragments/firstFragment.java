5d7dfa37271c3587b3b7e39588fe5471be4abcd0
Josh <43157130+joshdefreitas@users.noreply.github.com>
2020-07-28 12:47:48 -0700

    location updatable

--- app/src/main/java/com/example/weatherapp/Fragments/firstFragment.java
+++ app/src/main/java/com/example/weatherapp/Fragments/firstFragment.java
@@ -2,11 +2,16 @@ package com.example.weatherapp.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Bundle;
 
 import com.example.weatherapp.R;
 
@@ -15,6 +20,9 @@ import com.example.weatherapp.R;
  * A simple {@link Fragment} subclass.
  */
 public class firstFragment extends Fragment {
+    private TextView locText;
+    private Button locButton;
+    private String currentLoc = "London";
 
     public firstFragment() {
         // Required empty public constructor
@@ -24,11 +32,27 @@ public class firstFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_first, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        locText = v.findViewById(R.id.locView);
+        locButton = v.findViewById(R.id.locButton);
+
+        locButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                locText.setText(currentLoc);
+            }
+        });
+        return v;
     }
 
     public void setLocation(){
         //TODO implement location display
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 }
